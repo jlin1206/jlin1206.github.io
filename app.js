d974799ae68f23e095d5c0d72aca7ca6ea59f69e
jklin1206@gmail.com <jklin1206@gmail.com>
2021-04-19 17:32:55 -0500

    Projects

--- app.js
+++ app.js
@@ -9,12 +9,10 @@ menu.addEventListener('click', function(){
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -35,17 +33,3 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
-}
